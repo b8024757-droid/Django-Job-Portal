40ae84a100478d5ad28db4c54b218dabe1bf1524
Balaji P <b8024757@gmail.com>
2026-08-06 02:18:59 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,8 @@
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
+python create_admin.py
+
 exec gunicorn jobs.wsgi:application \
     --bind 0.0.0.0:8000 \
     --workers 2
