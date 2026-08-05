0f6c835e6d8db5ab361d3d98b42f5e6117d112b8
Balaji P <b8024757@gmail.com>
2026-08-06 02:08:52 +0530

    Create create_admin.py

---
+++ create_admin.py
@@ -0,0 +1,21 @@
+import os
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jobs.settings")
+django.setup()
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+email = "admin@hiremind.ai"
+password = "YourStrongPassword123"
+
+if not User.objects.filter(email=email).exists():
+    User.objects.create_superuser(
+        email=email,
+        password=password
+    )
+    print("Superuser created!")
+else:
+    print("Superuser already exists.")
